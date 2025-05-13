bdcb99ec31afe1e9b7ad607a3fd9ad2aad02ab2d
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-13 17:48:42 +0200

    check if animal is hungry

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from typing import List
 
 
 class Animal:
-
-
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(
+        self, name: str, appetite: int, is_hungry: bool = True
+    ) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -17,28 +17,26 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
+
 
 class Cat(Animal):
-
-
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
-        print(f"The hunt began!")
+        print("The hunt began!")
 
 
 class Dog(Animal):
-
-
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self):
-        print(f"The slippers delivered!")
+    def bring_slippers(self) -> None:
+        print("The slippers delivered!")
 
 
 def feed_animals(animals: List[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
+
+
